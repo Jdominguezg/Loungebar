ae147a8e00239f21c9f460a1f777574b1bd51117
Jose Manuel <jdominguezg3@gmail.com>
2019-03-18 13:44:05 +0000

    TODO
    Focus modal

---
+++ public/js/ckeditor/config.js
@@ -0,0 +1,23 @@
+/**
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+	config.removePluggins = 'easyimage';
+	config.removeButtons = 'SpecialChar,PageBreak,Flash,Table,Anchor,Language,CreateDiv,CopyFormatting,RemoveFormat,Form,Select,Button,Radio,ImageButton,HiddenField,Checkbox,TextField,Textarea,Scayt,SelectAll,Find,Replace,Cut,Copy,Paste,PasteText,PasteFromWord,Templates,NewPage,Preview,Print,Source,BidiLtr,BidiRtl,About,ShowBlocks,Maximize,Save,Iframe,Styles';
+};
--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -5,5 +5,5 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>LoungeBar</title>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-	<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/all.js') }}"></script>
 </head>
