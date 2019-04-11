9c244ccce20a7a16201626db3cf97a9e403ee1d2
Jose Manuel <jdominguezg3@gmail.com>
2019-04-11 13:43:56 +0100

    Doing Login Modal

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -4,6 +4,7 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>LoungeBar</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 	<script type="text/javascript" src="{{ asset('js/all.js') }}"></script>
 </head>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,17 +2,28 @@
 @section('content')
 <div class="index_background" style="background-image: url('{{ asset('imgs/IndexBackground/bg-'.(rand(1,25)).'.jpeg') }}')"></div>
 
-<section class="login_modal">
-	<h1 class="login_title light">Inicia Sesión</h1>
-
-	<div class="textfield">
-		<input type="text" name="username" id="username">
-		<label for="username">Usuario</label>
-	</div>
-	<div class="textfield">
-		<input type="password" name="password" id="password">
-		<label for="password">Contraseña</label>
-	</div>
-	
-</section>
+<h1 class="login_title light">Inicia Sesión</h1>
+<div class="flip_card">
+	<section class="login_modal front">
+		<div class="col-12">
+			<div class="textfield">
+				<input type="text" name="username" id="username">
+				<label for="username">Usuario</label>
+			</div>
+			<div class="textfield">
+				<input type="password" name="password" id="password">
+				<label for="password">Contraseña</label>
+			</div>
+			<button class="raised_button login_button"><span>Iniciar sesión</span></button>
+			<div class="register_now">
+				<div>¿No tienes cuenta todavía?</div>
+				<a href="#">Regístrate</a>
+			</div>
+			<button class="social_button" style="background-color: #4267b2; color: #fff;"><i class="col-1 fab fa-facebook-f"></i><span class="col-11 text_left">Iniciar con Facebook</span></button>
+			<button class="social_button" style="background-color: #1DA1F2; color:#fff;"><i class="col-1 fab fa-twitter"></i><span class="col-11 text_left">Iniciar con Twitter</span> </button>
+			<button class="social_button" style="background-color: #DD4B39; color:#fff;"><i class="col-1 fab fa-google"></i><span class="col-11 text_left">Iniciar con Google</span>  </button>
+		</div>
+	</section>
+	<section class="signin_modal back"></section>
+</div>
 @endsection
