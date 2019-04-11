1cf97dd005c2e23d3c14252948d00ff3a2f82863
Jose Manuel <jdominguezg3@gmail.com>
2019-04-11 09:14:22 +0100

    DOING  - Login  frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,18 @@
 @extends('principal')
 @section('content')
 <div class="index_background" style="background-image: url('{{ asset('imgs/IndexBackground/bg-'.(rand(1,25)).'.jpeg') }}')"></div>
-<section class="login_modal"></section>
+
+<section class="login_modal">
+	<h1 class="login_title light">Inicia Sesión</h1>
+
+	<div class="textfield">
+		<input type="text" name="username" id="username">
+		<label for="username">Usuario</label>
+	</div>
+	<div class="textfield">
+		<input type="password" name="password" id="password">
+		<label for="password">Contraseña</label>
+	</div>
+	
+</section>
 @endsection
