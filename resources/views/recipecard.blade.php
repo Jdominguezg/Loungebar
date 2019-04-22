f8e5268d97a667e20fda8c7e9ea4dbdec3c52b68
Jose Manuel <jdominguezg3@gmail.com>
2019-04-22 12:49:24 +0100

    DOING recipe cards

---
+++ resources/views/recipecard.blade.php
@@ -0,0 +1,17 @@
+@extends('principal')
+@section('content')
+<section style="margin-top:56px;">	
+	<div class="recipe_card">
+		<div class="recipe_img" style="background-image: url('{{asset('imgs/indexBackground/bg-1.jpeg')}}');">
+			<div class="user_info">
+				<div class="user_img"></div>
+				<div class="user_name"></div>
+			</div>
+			<div class="recipe_title">
+				Hambuerguesa cremosa
+			</div>
+		</div>
+
+	</div>
+</section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/addrecipe', function(){
 	return view('addrecipe');
 });
+
+Route::get('/recipecard', function(){
+	return view('recipecard');
+});
