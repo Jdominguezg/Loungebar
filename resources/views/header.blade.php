0bc9e0853204d61a4feab1eaef3a1a81fd843b63
Jose Manuel <jdominguezg3@gmail.com>
2019-04-23 13:15:47 +0100

    Doing recipe cards

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,9 +1,6 @@
 <header class="center">
-	<div class="col-2">
-		<i class="material-icons pointer">menu</i>
-	</div>
-	<div class="col-8">
-		<h1 class="light">LoungeBar - Admin</h1>
+	<div class="col-12 logo">
+		Tastefull
 	</div>
 	
 </header>
--- resources/views/recipecard.blade.php
+++ resources/views/recipecard.blade.php
@@ -1,17 +1,49 @@
 @extends('principal')
 @section('content')
 <section style="margin-top:56px;">	
+	@for ($i = 0; $i < 5 ; $i++)
 	<div class="recipe_card">
-		<div class="recipe_img" style="background-image: url('{{asset('imgs/indexBackground/bg-1.jpeg')}}');">
-			<div class="user_info">
-				<div class="user_img"></div>
-				<div class="user_name"></div>
-			</div>
-			<div class="recipe_title">
-				Hambuerguesa cremosa
-			</div>
+		<div class="recipe_card_header">
+			<a href="#">
+				<div class="user_info">
+					<div class="user_img" style="background-image: url('{{ asset('imgs/user.png') }}')"></div>
+					<div class="user_name medium">Pepe el Cojo</div>
+				</div>
+			</a>
+			<a href="#">
+				<i class="material-icons share">share</i>
+			</a>
 		</div>
+		<a href="#">
+			<div class="recipe_img" style="background-image: url('{{asset('imgs/indexBackground/bg-'.($i+1).'.jpeg')}}');">
 
+				<div class="recipe_header">
+					<div class="recipe_title">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</div>
+					<div class="recipe_duration">
+						<i class="material-icons">access_time</i>
+						<span style="margin:0 5px" class="recipe_duration_time">3 Horas y 2 minutos</span>
+					</div>
+				</div>
+				<div class="show_more">ver más</div>
+			</div>
+		</a>
+		<div class="recipe_actions">
+			<div class="rate"><i class="material-icons">favorite_outlined</i></div>
+			<div class="comment"><i class="material-icons">chat_bubble_outline</i></div>
+		</div>
+		<div class="recipe_rate">
+			
+		</div>
 	</div>
+	@endfor
+	
+
 </section>
 @endsection
